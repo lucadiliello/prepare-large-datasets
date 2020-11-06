a9c3a6950dc9eb1c5e9d10ac080d86fb5565112a
lucadiliello <luca.diliello@unitn.it>
2020-11-06 10:43:31 +0100

    fixed typos in test_dataset

--- test_dataset.py
+++ test_dataset.py
@@ -1,3 +1,4 @@
+from itertools import count
 import os
 from argparse import ArgumentParser
 import csv
@@ -16,8 +17,8 @@ if __name__ == "__main__":
                         help="List of input wikipedia processed dumps with one sentence per line")
     parser.add_argument('--tokenizer', type=str, default=None, required=False,
                         help="Path of some pre-trained tokenizer")
-    parser.add_argument('--limit', default=20000, required=False, help="Limit number of rows to be faster")
-    parser.add_argument('--column', default=1, required=False, help="Column id of the sentences")
+    parser.add_argument('--limit', default=20000, required=False, type=int, help="Limit number of rows to be faster")
+    parser.add_argument('--column', default=1, required=False, type=int, help="Column id of the sentences")
 
     # get NameSpace of paramters
     args = parser.parse_args()
@@ -30,12 +31,17 @@ if __name__ == "__main__":
         tokenizer = None
 
     lengths = []
+    counter = 0
 
     with open(args.input_file) as in_file:
         reader = csv.reader(in_file, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for line in tqdm(reader, desc="Reading file", total=args.limit):
 
             line = line[args.column]
+            if counter >= args.limit:
+                break
+
+            counter += 1
 
             if tokenizer is None:
                 lengths.append(len(line.strip().split(" ")))
