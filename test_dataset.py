63b9f27efd881a74abbda001abfc94e8c8e82433
lucadiliello <luca.diliello@unitn.it>
2020-11-06 10:37:05 +0100

    fixed typos in test_dataset

--- test_dataset.py
+++ test_dataset.py
@@ -1,9 +1,7 @@
 import os
 from argparse import ArgumentParser
 import csv
-import json
-import numpy
-import transformers
+import numpy as np
 from tqdm import tqdm
 from transformers.tokenization_auto import AutoTokenizer
 
@@ -14,17 +12,17 @@ if __name__ == "__main__":
     parser = ArgumentParser("Measure average line length in tokens or words")
 
     # Global level parameters
-    parser.add_argument('-i', '--input_file', type=str, required=True, nargs='+',
+    parser.add_argument('-i', '--input_file', type=str, required=True,
                         help="List of input wikipedia processed dumps with one sentence per line")
     parser.add_argument('--tokenizer', type=str, default=None, required=False,
                         help="Path of some pre-trained tokenizer")
     parser.add_argument('--limit', default=20000, required=False, help="Limit number of rows to be faster")
+    parser.add_argument('--column', default=1, required=False, help="Column id of the sentences")
 
     # get NameSpace of paramters
     args = parser.parse_args()
 
     assert os.path.isfile(args.input_file), f"File {args.input_file} does not exist"
-    assert not args.lang_file or os.path.isfile(args.lang_file), f"file {args.lang_file} does not exist"
 
     if args.tokenizer is not None:
         tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
@@ -36,7 +34,9 @@ if __name__ == "__main__":
     with open(args.input_file) as in_file:
         reader = csv.reader(in_file, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for line in tqdm(reader, desc="Reading file", total=args.limit):
-            
+
+            line = line[args.column]
+
             if tokenizer is None:
                 lengths.append(len(line.strip().split(" ")))
             else:
